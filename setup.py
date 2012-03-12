c450e3db85a02825afa930980bb205cf1c21e128
Zach Williams <hey@zachwill.com>
2012-03-11 18:29:00 -0700

    Maintain the top level node namespace

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
 
 setup(
     name="relaxml",
-    version="0.1.1",
+    version="0.1.2",
     url="http://github.com/zachwill/relaxml",
     author="Zach Williams",
     author_email="hey@zachwill.com",
