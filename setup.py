3902ee796a918495dd7b789b3a8908d4ea757ac6
Zach Williams <hey@zachwill.com>
2012-06-13 10:27:02 -0700

    Update version via @alekzvik pull request

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
 
 setup(
     name="relaxml",
-    version="0.1.2",
+    version="0.1.3",
     url="http://github.com/zachwill/relaxml",
     author="Zach Williams",
     author_email="hey@zachwill.com",
